625f5fbee257f496f45fdfdad1740da4c195804a
Richard Haythornthwaite <richardhaythornthwaite@hotmail.co.uk>
2023-07-02 15:09:41 +0100

    add get person by name, patch person

--- src/server.go
+++ src/server.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"net/http"
-    "github.com/gin-gonic/gin"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
 type person struct {
+    ID        int64    `json:"id"`
 	FirstName string `json:"firstname"`
 	LastName  string `json:"lastname"`
-	Age       int    `json:"age"`
+	Age       int64    `json:"age"`
 }
 
 var persons = []person{
-	{FirstName: "John", LastName: "Smith", Age: 25},
+	{ID: 0, FirstName: "John", LastName: "Smith", Age: 25},
+	{ID: 1, FirstName: "Steven", LastName: "Gerrard", Age: 46},
 }
 
 type PersonServer struct {
@@ -34,11 +38,45 @@ func postPersons(c *gin.Context) {
     c.IndentedJSON(http.StatusCreated, newPerson)
 }
 
+func patchPerson(c *gin.Context) {
+    id, _ := strconv.ParseInt(c.Param("id"), 10, 64)
+    var updatedPerson person
+
+    if err := c.BindJSON(&updatedPerson); err != nil {
+        return
+    }
+
+    newpersons := []person{}
+    for _, person := range persons {
+        if person.ID == id {
+            newpersons = append(newpersons, updatedPerson)
+        } else {            
+            newpersons = append(newpersons, person)
+        }
+    }
+    persons = newpersons
+    c.IndentedJSON(http.StatusCreated, updatedPerson)
+}
+
+func getPersonByFirstName(c *gin.Context) {
+    firstname := c.Param("firstname")
+
+    for _, person := range persons {
+        if person.FirstName == firstname {
+            c.IndentedJSON(http.StatusOK, person)
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "person not found"})
+}
+
 func main() {
 
     router := gin.Default()
     router.GET("/persons", getPersons)
     router.POST("/persons", postPersons)
+    router.GET("/persons/:firstname", getPersonByFirstName)
+    router.PATCH("/persons", patchPerson)
 
     router.Run("localhost:8080")
 }
