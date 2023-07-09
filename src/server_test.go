f4e3697c3bb19baabe4bd501a3abd39c7a973e14
Richard Haythornthwaite <richardhaythornthwaite@hotmail.co.uk>
2023-07-09 19:33:33 +0100

    test update

--- src/server_test.go
+++ src/server_test.go
@@ -1,2 +1,61 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+	"testing"
+)
+
+func TestPOST(t *testing.T) {
+    resp, err := http.Get("http://localhost:8080/persons")
+	if err != nil {
+		t.Errorf("Failed GET")
+	}
+    if resp.Status != "200 OK" {
+		t.Errorf("Resp status not OK")
+	}
+    defer resp.Body.Close()
+}
+
+func TestGET(t *testing.T) {
+	resp, err := http.Get("http://localhost:8080/persons")
+	if err != nil {
+		t.Errorf("Failed GET")
+	}
+    if resp.Status != "200 OK" {
+		t.Errorf("Resp status not OK, Status: %s",resp.Status)
+	}
+    defer resp.Body.Close()
+}
+
+func TestGETByID(t *testing.T) {
+	resp, err := http.Get("http://localhost:8080/persons/John")
+	if err != nil {
+		t.Errorf("Failed GET")
+	}
+    if resp.Status != "200 OK" {
+		t.Errorf("Resp status not OK, Status: %s",resp.Status)
+	}
+    defer resp.Body.Close()
+}
+
+func TestPATCH(t *testing.T) {
+
+}
+
+func setup(router *gin.Engine) {
+	router.GET("/persons", getPersons)
+	router.POST("/persons", postPersons)
+	router.GET("/persons/:firstname", getPersonByFirstName)
+	router.PATCH("/persons/:id", patchPerson)
+
+	router.Run("localhost:8080")
+}
+
+func TestMain(m *testing.M) {
+	router := gin.Default()
+	go setup(router)
+	code := m.Run()
+	os.Exit(code)
+}
